967a2f9c483672818680c0e3ed24d31e1f61fb35
Samuel Price <sjp@Samuels-MBP.lan>
2024-03-22 16:52:39 -0400

    signin

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,26 +1,37 @@
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_list"] = Post.objects.order_by(
-            "created_on").reverse()
-        return context
-    
 class PostDetailView(DetailView):
-    template_name = "posts/detail.html"
-    model = Post
-    
+        template_name = "posts/detail.html"
+        model = Post
+        
 class PostCreateView(CreateView):
     template_name = "posts/new.html"
     model = Post
-    fields = ['title', 'subtitle', 'author', 'body']
+    fields = ["title", "subtitle", "author", "body"]
+
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "author", "body"]
+
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+
